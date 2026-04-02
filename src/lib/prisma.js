f1d562d25f9ada4fc6e8f76cdeeee78c8e59d185
darkmatter <vikasanand.darkmatter@gmail.com>
2026-04-03 00:56:55 +0530

    Deploy Serverless

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,13 +1,38 @@
+// import pkg from '@prisma/client';
+// const { PrismaClient } = pkg;
+// import { PrismaPg } from '@prisma/adapter-pg';
+// import { env } from '../config/env.js';
+
+// const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
+
+// const prisma = new PrismaClient({
+//   adapter,
+//   log: ['error', 'warn'],
+// });
+
+// export default prisma;
+
 import pkg from '@prisma/client';
 const { PrismaClient } = pkg;
+
 import { PrismaPg } from '@prisma/adapter-pg';
 import { env } from '../config/env.js';
 
-const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
+const globalForPrisma = globalThis;
 
-const prisma = new PrismaClient({
-  adapter,
-  log: ['error', 'warn'],
+const adapter = new PrismaPg({
+  connectionString: env.DATABASE_URL,
 });
+
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+    log: ['error'],
+  });
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
